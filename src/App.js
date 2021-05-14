a0b35e9973a5c95b044785cb87e9d7474777652d
chongyuan.guo <chongyuan.guo@qunar.com>
2021-05-14 23:56:40 +0800

    ..

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App test">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React Test        </a>
       </header>
     </div>
   );
