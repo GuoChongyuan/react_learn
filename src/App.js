43ced46eed951d3ffcdf0bf05cd94d44a7ef6296
chongyuan.guo <chongyuan.guo@chongyuguodeMBP.lan>
2021-05-15 20:10:34 +0800

    change word

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.svg';
+import testimg from './test.jpg';
 import './App.css';
 
 function App() {
   return (
+
     <div className="App test">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+              杨栋 老婆大人最美丽
+          <p/>
+          郭崇源最爱杨栋老婆大人
+        <img src={testimg} className="App-logo" alt="logo" />
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test1        </a>
+           </a>
       </header>
     </div>
   );
